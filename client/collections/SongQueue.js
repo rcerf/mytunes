290951ed914882ff2d05d406fac2f80ba868c793
rcerf <rickcerf@gmail.com>
2013-10-09 17:20:20 -0700

    plays first song added to songQueue, does nothing when more than one song is present in the songQueue, removes song from songQueue when song ends.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,23 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
+
   initialize: function(){
+    //console.log("init", this);
+    this.on("add", function(){
+      if (this.models.length === 1) {
+        this.playFirst();
+      }
+    });
+    this.on("ended", function(){
+      this.removeSong();
+    });
+  },
+
+  playFirst: function(){ this.models[0].play(); },
+
+  removeSong: function(){
+    this.shift();
   }
 
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,6 +2,9 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    this.collection.on('add', function() {
+      console.log('hi');
+    });
   },
 
   render: function() {
